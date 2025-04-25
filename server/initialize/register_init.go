9e58a756e4765fa83872f932ee7e740e25046e3d
Perlou <perloukevin@gmail.com>
2025-04-25 23:08:47 +0800

    feat init

---
+++ server/initialize/register_init.go
@@ -0,0 +1,11 @@
+package initialize
+
+// TODO
+// import (
+// 	_ "github.com/perlou/go-vue-admin/server/source/example"
+// 	_ "github.com/perlou/go-vue-admin/server/source/system"
+// )
+
+// func init() {
+// 	// do nothing,only import source package so that inits can be registered
+// }
---
+++ server/initialize/timer.go
@@ -0,0 +1,18 @@
+package initialize
+
+// TODO
+
+// import (
+// 	"github.com/perlou/go-vue-admin/server/global"
+// 	"github.com/robfig/cron/v3"
+// )
+
+// func Timer() {
+// 	go func ()  {
+// 		var option []cron.Option
+// 		option = append(option, cron.WithSeconds())
+// 		_, err := global.GVA_Timer.AddTaskByFunc("ClearDB", "@daily", func() {
+// 			err := task.ClearTable
+// 		})
+// 	}()
+// }
